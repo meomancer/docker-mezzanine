7789d195245a5463d95d90641211f137c3880070
meomancer <meomancer@gmail.com>
2017-01-04 12:23:10 +0700

    update setup

--- mezzanine_kartoza/config/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'Irwan Fathurrahman <irwan@kartoza.com>'
-__date__ = '23/08/16'
-__license__ = "GPL"
-__copyright__ = 'kartoza.com'
-
-from .poll_twitter import poll_twitter
--- setup.py
+++ setup.py
@@ -7,12 +7,9 @@ setup(
 
     install_requires=[
         'cartridge',
-        'celery',
         'Django>=1.8,<1.9',
         'django-compressor',
-        'django-sentry==1.13.5',
         'filebrowser-safe==0.4.5',
-        'geopy==0.95.1',
         'html5lib==0.9999999',
         'icalendar==3.0.1b2',
         'mezzanine',
@@ -21,11 +18,7 @@ setup(
         'mezzanine-references',
         'mezzanine-slides',
         'OWSLib==0.12',
-        'psycopg2',
-        'raven==5.8.1',
-        'requests==2.0.0',
-        'Mezzanine >= 3.1.10',
-        'six >= 1.5.2'],
+        'Mezzanine >= 3.1.10'],
 
     dependency_links=[
         'git+https://github.com/dimasciput/mezzanine-careers.git@develop',
